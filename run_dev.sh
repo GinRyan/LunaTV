88624e2c1ecb6f0027e40562d600d93ad7de372f
Kuma <lp8800lz@163.com>
2026-03-03 10:06:04 +0800

    增加了redis连接

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,7 @@
 # ./update_modules.sh
 export USERNAME=admin
 export PASSWORD=123456
-export NEXT_PUBLIC_STORAGE_TYPE=kvrocks
+export NEXT_PUBLIC_STORAGE_TYPE=redis
 export KVROCKS_URL=redis://127.0.0.1:6666
+export REDIS_URL=redis://127.0.0.1:6379
 pnpm run dev
